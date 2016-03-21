a76a0691ff387db6979975786f4481db40ef425c
Klaus Myrseth <klaus.myrseth@acando.no>
2016-03-21 11:01:08 +0100

    Fixed the config

--- gui/app/config.js
+++ gui/app/config.js
@@ -34,21 +34,21 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'React Redux Example',
-    description: 'All the modern best practices in one example.',
+    title: 'MUSIT',
+    description: 'MUSIT - the Norwegian museum database initiative.',
     head: {
-      titleTemplate: 'React Redux Example: %s',
+      titleTemplate: 'MUSIT: %s',
       meta: [
         {name: 'description', content: 'All the modern best practices in one example.'},
         {charset: 'utf-8'},
-        {property: 'og:site_name', content: 'React Redux Example'},
+        {property: 'og:site_name', content: 'MUSIT'},
         {property: 'og:image', content: 'https://react-redux.herokuapp.com/logo.jpg'},
         {property: 'og:locale', content: 'en_US'},
-        {property: 'og:title', content: 'React Redux Example'},
-        {property: 'og:description', content: 'All the modern best practices in one example.'},
+        {property: 'og:title', content: 'MUSIT'},
+        {property: 'og:description', content: 'MUSIT - the Norwegian museum database initiative.'},
         {property: 'og:card', content: 'summary'},
-        {property: 'og:site', content: '@erikras'},
-        {property: 'og:creator', content: '@erikras'},
+        {property: 'og:site', content: '@musit-project'},
+        {property: 'og:creator', content: '@musit-project'},
         {property: 'og:image:width', content: '200'},
         {property: 'og:image:height', content: '200'}
       ]
