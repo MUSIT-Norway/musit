0628ec7b7ab087584c17a67308bcb33b508f707e
Klaus Myrseth <klaus.myrseth@acando.no>
2016-03-21 09:44:04 +0100

    Added license comments that I forgot.

--- gui/app/components/NotFound/index.js
+++ gui/app/components/NotFound/index.js
@@ -1,10 +1,28 @@
+/*
+ *  MUSIT is a museum database to archive natural and cultural history data.
+ *  Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License,
+ *  or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 import React from 'react';
 
 const NotFound = () => {
   return (
     <div className="container">
-      <h1>Doh! 404!</h1>
-      <p>These are <em>not</em> the droids you are looking for!</p>
+      <h1>Not found! 404!</h1>
+      <p>The route you are looking for does not exist!</p>
     </div>
   )
 }
