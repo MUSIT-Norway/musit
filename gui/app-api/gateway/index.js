691a201aa8e5a80ead1f64f5c0b8930c569c0b3a
Klaus Myrseth <klaus.myrseth@acando.no>
2016-04-20 18:05:02 +0200

    Added basic gateway code without authentication.

--- gui/app-api/gateway/index.js
+++ gui/app-api/gateway/index.js
@@ -20,6 +20,7 @@ export default class APIGateway {
     let splitArray = uriArray[0].split('/')
     this.registry.get(splitArray[2], (err, service) => {
       if(!err && service) {
+        console.log('-- calling service: '+service.name)
         // Fix options
         let options = ''
         if (uriArray[1] && uriArray[1].length > 0) {
@@ -38,13 +39,13 @@ export default class APIGateway {
 
         // Pipe the request to the microservices
         console.log("Forwarding service call in pipe: "+newUrl)
-        try {
-          req.pipe(request(newUrl).pipe(res))
-        } catch (err) {
-          console.error('MS ERROR:', pretty.render(err))
-          res.status(err.status || 500).json(err)
-        }
+        let newReq = request(newUrl)
+        newReq.on('error', (err) => {
+          console.error('MS ERROR:', err)
+          res.status(500).json(err)
+        })
 
+        req.pipe(newReq).pipe(res)
       } else {
         console.error('MS ERROR:', pretty.render(err))
         res.status(404).json(err)
