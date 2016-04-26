2623f17064b2fada9b6c40be7eac42c905cf9bb6
Klaus Myrseth <klaus.myrseth@acando.no>
2016-04-26 13:11:56 +0200

    Added timeout to test.

--- service_musit_thing/test/MusitThing_TestSuite.scala
+++ service_musit_thing/test/MusitThing_TestSuite.scala
@@ -5,10 +5,11 @@
 import no.uio.musit.microservice.service_musit_thing.dao.MusitThingDao
 import no.uio.musit.microservice.service_musit_thing.domain.MusitThing
 import no.uio.musit.microservices.common.linking.LinkService
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{PatienceConfiguration, ScalaFutures}
 import org.scalatest.{FunSuite, Matchers}
 import play.api.test.FakeApplication
 import play.api.test.Helpers._
+import scala.concurrent.duration._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -20,6 +21,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
     ("slick.dbs.default.db.url" , "jdbc:h2:mem:play-test"),
     ("evolutionplugin" , "enabled")
   )
+  val timeout = PatienceConfiguration.Timeout(1 seconds)
 
   test("testInsertMusitThing") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
@@ -29,18 +31,16 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
       svar.onFailure{
         case ex => fail("Insert failed")
       }
-      whenReady(svar) { things =>
+      whenReady(svar, timeout) { things =>
         assert (things.length == 4)
       }
     }
   }
-
-
-
+  
   test("getDisplayName_kjempeTall") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getDisplayName(6386363673636335366L)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == None)
       }
     }
@@ -49,7 +49,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getDisplayName_Riktig") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getDisplayName(2)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == Some("Kniv7"))
       }
     }
@@ -58,7 +58,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getDisplayName_TalletNull") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getDisplayName(0)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == None)
       }
     }
@@ -67,7 +67,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getDisplayID_kjempeTall") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getDisplayID(6386363673636335366L)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == None)
       }
     }
@@ -76,7 +76,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getDisplayID_Riktig") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getDisplayID(2)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == Some("C2"))
       }
     }
@@ -85,7 +85,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getDisplayID_TalletNull") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getDisplayID(0)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == None)
       }
     }
@@ -94,7 +94,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getById_kjempeTall") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getById(6386363673636335366L)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == None)
       }
     }
@@ -103,7 +103,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getById__Riktig") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getById(1)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == Some(MusitThing(1,"C1","Øks5", Seq(LinkService.self("/v1/1")))))
       }
     }
@@ -112,7 +112,7 @@ class MusitThing_TestSuite extends FunSuite with Matchers with ScalaFutures {
   test("getById__TalletNull") {
     running(FakeApplication(additionalConfiguration = additionalConfiguration)) {
       val svar = MusitThingDao.getById(0)
-      whenReady(svar) { thing =>
+      whenReady(svar, timeout) { thing =>
         assert (thing == None)
       }
     }
