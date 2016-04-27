efb78943926afe3fea37f09b0d1d566a135ec7cc
Stein Olsen <stein.olsen@usit.uio.no>
2016-04-27 15:11:06 +0200

    Added new Actor microservice boiler plate as copy of MusitThing

---
+++ service_actor/app/no/uio/musit/microservice/actor/resource/ActorResource_V1.scala
@@ -0,0 +1,122 @@
+/*
+ * MUSIT is a museum database to archive natural and cultural history data.
+ * Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package no.uio.musit.microservice.actor.resource
+
+import no.uio.musit.microservice.actor.dao.ActorDao
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import io.swagger.annotations._
+import no.uio.musit.microservice.actor.domain.Actor
+import no.uio.musit.microservice.actor.service.ActorService
+import play.api.Logger
+import play.api.mvc._
+import play.api.libs.json._
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+@Api(value = "/api/musitThing", description = "MusitTHing resource, showing how you can put simple methods straight into the resource and do complex logic in traits outside.")
+class ActorResource_V1 extends Controller with ActorService {
+  //val musit_thing_Dao = new MusitThingDao
+
+  @ApiOperation(value = "MusitThing operation - lists all musitThings", notes = "simple listing in json", httpMethod = "GET")
+  def list = Action.async { req => {
+    //req.getQueryString("filter")
+    ActorDao.all.map(musitThing =>
+      Ok(Json.toJson(musitThing))
+    )}
+  }
+
+
+  @ApiOperation(value = "MusitThing operation - get a spesific musitThing", notes = "simple listing in json", httpMethod = "GET")
+  def getById(id:Long) = Action.async { request => {
+
+    def wrapWithOkOrFailString(eventualMaybeResult: Future[Option[String]]) = {
+      eventualMaybeResult.map(
+        maybeResult=>maybeResult.map(
+          result=>Ok(Json.toJson(result))
+        ).getOrElse(
+          NotFound(s"Didn't find object with id: $id")
+        )
+      )
+    }
+    def wrapWithOkOrFailThing(eventualMaybeResult: Future[Option[Actor]]) = {
+      eventualMaybeResult.map(
+        maybeResult=>maybeResult.map(
+          result=>Ok(Json.toJson(result))
+        ).getOrElse(
+          NotFound(s"Didn't find object with id: $id")
+        )
+      )
+    }
+
+
+
+    val filterListe = Seq("displayid", "displayname")
+
+    val filter = extractFilterFromRequest(request)
+    if (filter.length == 1) {
+      filter(0).toLowerCase match {
+        case "displayid" => wrapWithOkOrFailString(ActorDao.getDisplayID(id)) //Json.toJson(MusitThingDao.getDisplayID(id).map( )))
+        case "displayname" =>  wrapWithOkOrFailString(ActorDao.getDisplayName(id))
+        case whatever => Future(BadRequest(s"Unknown filter:$whatever"))
+      }
+
+    } else if (filter.length > 1) {
+      /*val json = Json.toJson(MusitThingDao.getById(1).map( thing => thing))
+      Logger.debug(json.toString)
+      val displayId = json \ "displayid"*/
+      Future(BadRequest("Filter can not contain more then one attribute"))
+    } else {
+      wrapWithOkOrFailThing(ActorDao.getById(id))
+    }
+  }
+  }
+
+
+
+
+  @ApiOperation(value = "MusitThing operation - inserts an MusitThingTuple", notes = "simple json parsing and db insert", httpMethod = "POST")
+  def add = Action.async(BodyParsers.parse.json) { request =>
+    val musitThingResult:JsResult[Actor] = request.body.validate[Actor]
+    musitThingResult match {
+      case s:JsSuccess[Actor] => {
+        val musitThing = s.get
+        val newThingF = ActorDao.insert(musitThing)
+        newThingF.map { newThing =>
+          Created(Json.toJson(newThing))
+        }
+      }
+      case e:JsError => Future(BadRequest(Json.obj("status" -> "Error", "message" -> JsError.toJson(e))))
+    }
+  }
+
+  def extractFilterFromRequest(request: Request[AnyContent]): Array[String] = {
+    request.getQueryString("filter") match {
+      case Some(filterString) => "^\\[(\\w*)\\]$".r.findFirstIn(filterString) match {
+        case Some(str) => str.split(",")
+        case None => Array.empty[String]
+      }
+      case None => Array.empty[String]
+    }
+  }
+
+}
+
+
+
---
+++ service_actor/src/it/scala/ActorIntegrationTest.scala
@@ -0,0 +1,45 @@
+/*
+ *  MUSIT is a museum database to archive natural and cultural history data.
+ *  Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License,
+ *  or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+import org.scalatest.concurrent.{PatienceConfiguration, ScalaFutures}
+import org.scalatest.{FunSuite, Matchers}
+import org.scalatestplus.play.{OneAppPerSuite, OneServerPerSuite, PlaySpec}
+import play.api.libs.ws.WS
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json._
+
+import scala.concurrent.duration._
+
+/**
+ * add your integration spec here.
+ * An integration test will fire up a whole play application in a real (or headless) browser
+ */
+class ActorIntegrationTest extends PlaySpec with OneServerPerSuite with ScalaFutures {
+  implicit override lazy val app = new GuiceApplicationBuilder().build()
+  val timeout = PatienceConfiguration.Timeout(1 seconds)
+
+  "MusitThing integration " must {
+    "get by id" in {
+      val future = WS.url(s"http://localhost:$port/v1/1").get()
+      whenReady(future, timeout) { response =>
+        val json = Json.parse(response.body)
+        assert((json \ "id").get.toString() == "1")
+      }
+    }
+  }
+}
