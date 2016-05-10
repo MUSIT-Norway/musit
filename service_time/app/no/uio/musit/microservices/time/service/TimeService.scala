30e100994cfa4fb9e6bac3299c4173d24dbbaa26
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2016-05-10 13:12:33 +0200

    Musark 108 (#23)
    * MUSARK-108 Refactoring time service + adding bindable filter and search parameter for routes files + overriding error handler locally for testing
    
    * MUSARK-108 Going away from throwing exceptions, and instead using Option and Either
    
    * MUSARK-108 Moving domain classes to common
    
    * MUSARK-108 Implementing code review fixes after meeting

---
+++ service_time/app/no/uio/musit/microservices/time/service/TimeService.scala
@@ -0,0 +1,44 @@
+/*
+ * MUSIT is a museum database to archive natural and cultural history data.
+ * Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License,
+ * or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package no.uio.musit.microservices.time.service
+
+import no.uio.musit.microservices.common.domain.{MusitError, MusitFilter}
+import no.uio.musit.microservices.time.domain._
+import org.joda.time.DateTime
+import org.joda.time.DateTime.now
+
+trait TimeService {
+  
+  def convertToNow(maybeFilter: Option[MusitFilter]): Either[MusitError, MusitTime] =
+    maybeFilter.map(f => resolveNow(f)).getOrElse(Right(fromDateTime(now)))
+
+  def resolveNow(filter: MusitFilter): Either[MusitError, MusitTime] = filter match {
+    case MusitFilter(List("time")) => Right(MusitTime(time = Some(now.toLocalTime)))
+    case MusitFilter(List("date")) => Right(MusitTime(date = Some(now.toLocalDate)))
+    case MusitFilter(List("date","time")) => Right(fromDateTime(now))
+    case _ => Left(MusitError(message = "Only supports empty filter or filter on time, date or time and date"))
+  }
+  
+  def fromDateTime(dateTime: DateTime): MusitTime =
+    MusitTime(
+      date = Some(dateTime.toLocalDate),
+      time = Some(dateTime.toLocalTime)
+    )
+
+}
