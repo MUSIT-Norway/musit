84109a1acb4a0ab85f730a6848fa98f1b86078e5
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-05-13 15:29:02 +0200

    MUSARK-130 Adding a basic skeleton variant of the dummy storage unit list

---
+++ gui/app/components/storageunits/StorageUnitList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class StorageUnitList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.units.map( u =>
+                    <span>{u.name}</span>
+                )}
+            </div>
+        );
+    }
+}
+
+StorageUnitList.propTypes =  {
+    units: React.PropTypes.arrayOf(React.PropTypes.shape({
+        name: React.PropTypes.string.isRequired
+    })).isRequired
+};
+
+export default StorageUnitList
