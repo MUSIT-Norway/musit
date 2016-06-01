e8fbbf9b49aa97905ac4fe9a2a828d5e8dd3b522
sveigl <s.g.glenndal@usit.uio.no>
2016-06-01 13:21:44 +0200

    Musark 126 (#42)
    * Created environment options component
    
    * Added correct binding and local state manipulation
    
    * Added correct use of Bootstrap Grid, for 4 screensizes
    
    * Deleted temporary copy

---
+++ gui/app/components/storageunits/EnvironmentOptions.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { Checkbox, ControlLabel, Grid, Row, Col } from 'react-bootstrap'
+export default class EnvironmentOptions extends React.Component {
+  static propTypes = {
+    unit: React.PropTypes.shape({
+      skallsikring: React.PropTypes.bool,
+      tyverisikring: React.PropTypes.bool,
+      brannsikring: React.PropTypes.bool,
+      vannskaderisiko: React.PropTypes.bool,
+      rutinerBeredskap: React.PropTypes.bool,
+      luftfuktighet: React.PropTypes.bool,
+      lysforhold: React.PropTypes.bool,
+      temperatur: React.PropTypes.bool,
+      preventivKonservering: React.PropTypes.bool,
+    }),
+    updateSkallsikring: React.PropTypes.func.isRequired,
+    updateTyverisikring: React.PropTypes.func.isRequired,
+    updateBrannsikring: React.PropTypes.func.isRequired,
+    updateVannskaderisiko: React.PropTypes.func.isRequired,
+    updateRutinerBeredskap: React.PropTypes.func.isRequired,
+    updateLuftfuktighet: React.PropTypes.func.isRequired,
+    updateLysforhold: React.PropTypes.func.isRequired,
+    updateTemperatur: React.PropTypes.func.isRequired,
+    updatePreventivKonservering: React.PropTypes.func.isRequired,
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Row styleClass="row-centered">
+          <Col lg={6} md={6} sm={6} xs={12}>
+            <ControlLabel>Sikring</ControlLabel>
+
+            <Checkbox
+              checked={this.props.unit.skallsikring}
+              onChange={(event) => this.props.updateSkallsikring(event.target.checked)}
+            >
+              Skallsikring
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.tyverisikring}
+              onChange={(event) => this.props.updateTyverisikring(event.target.checked)}
+            >
+              Tyverisikring
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.brannsikring}
+              onChange={(event) => this.props.updateBrannsikring(event.target.checked)}
+            >
+              Brannsikring
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.vannskaderisiko}
+              onChange={(event) => this.props.updateVannskaderisiko(event.target.checked)}
+            >
+              Vannskaderisiko
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.rutinerBeredskap}
+              onChange={(event) => this.props.updateRutinerBeredskap(event.target.checked)}
+            >
+              Rutiner/beredskap
+            </Checkbox>
+          </Col>
+          <Col lg={6} md={6} sm={6} xs={12}>
+            <ControlLabel>Bevaring</ControlLabel>
+
+            <Checkbox
+              checked={this.props.unit.luftfuktighet}
+              onChange={(event) => this.props.updateLuftfuktighet(event.target.checked)}
+            >
+              Luftfuktighet
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.lysforhold}
+              onChange={(event) => this.props.updateLysforhold(event.target.checked)}
+            >
+              Lysforhold
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.temperatur}
+              onChange={(event) => this.props.updateTemperatur(event.target.checked)}
+            >
+              Temperatur
+            </Checkbox>
+            <Checkbox
+              checked={this.props.unit.preventivKonservering}
+              onChange={(event) => this.props.updatePreventivKonservering(event.target.checked)}
+            >
+              Preventiv Konservering
+            </Checkbox>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+          }
--- gui/app/containers/example-view/index.js
+++ gui/app/containers/example-view/index.js
@@ -17,8 +17,9 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import React, { Component } from 'react';
-import TextField from '../../components/musittextfield';
+import React, { Component } from 'react'
+import TextField from '../../components/musittextfield'
+import Options from '../../components/storageunits/EnvironmentOptions'
 import { Panel, Form, Grid, Row, PageHeader, Col } from 'react-bootstrap'
 
 export default class ExampleView extends Component {
@@ -44,6 +45,17 @@ export default class ExampleView extends Component {
         note: '',
         areal: '',
         type: 'Rom'
+      },
+      sikringBevaring: {
+        skallsikring: false,
+        tyverisikring: true,
+        brannsikring: true,
+        vannskaderisiko: false,
+        rutinerBeredskap: false,
+        luftfuktighet: false,
+        lysforhold: false,
+        temperatur: false,
+        preventivKonservering: false
       }
     }
 
@@ -118,6 +130,29 @@ export default class ExampleView extends Component {
                 </Col>
               </Row>
             </Grid>
+          </Panel>
+          <Panel>
+            <Options
+              unit={this.state.sikringBevaring}
+              updateSkallsikring={(skallsikring) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, skallsikring } })}
+              updateTyverisikring={(tyverisikring) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, tyverisikring } })}
+              updateBrannsikring={(brannsikring) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, brannsikring } })}
+              updateVannskaderisiko={(vannskaderisiko) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, vannskaderisiko } })}
+              updateRutinerBeredskap={(rutinerBeredskap) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, rutinerBeredskap } })}
+              updateLuftfuktighet={(luftfuktighet) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, luftfuktighet } })}
+              updateLysforhold={(lysforhold) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, lysforhold } })}
+              updateTemperatur={(temperatur) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, temperatur } })}
+              updatePreventivKonservering={(preventivKonservering) =>
+                this.setState({ sikringBevaring: { ...this.state.sikringBevaring, preventivKonservering } })}
+            />
           </Panel>
         </main>
       </div>
