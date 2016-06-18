45dde770774df415f6dd63eba2fa91d5980ff4ea
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-06-18 07:32:21 +0200

    Removing static files + gui folder + improving index.html

--- gui/app/components/formfields/index.js
+++
@@ -1,6 +0,0 @@
-import MusitDropDownField from './musitdropdownfield'
-import MusitField from './musitfield'
-import MusitTextArea from './musittextarea'
-import MusitTextField from './musittextfield'
-
-export { MusitDropDownField, MusitField, MusitTextArea, MusitTextField }
