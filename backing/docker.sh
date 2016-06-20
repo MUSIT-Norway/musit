8c272e0c76b58da24edf0b62447d6be7833e60bf
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-06-20 07:53:28 +0200

    Making docker script more resiliant

--- backing/docker.sh
+++ backing/docker.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name musit-backing-docker -v "$PWD":/usr/src/app -w /usr/src/app node:4 rm -rf node_modules && npm install && npm start
+docker run -it --rm --name musit-backing -v "$PWD":/usr/src/app -w /usr/src/app node:5 rm -rf node_modules && npm install && npm start
--- frontend/docker.sh
+++ frontend/docker.sh
@@ -1,2 +1,4 @@
+docker stop musit-frontend
+docker rm musit-frontend
 docker build -t musit/frontend .
-docker run -v $(pwd)/src:/usr/src/app/src -v $(pwd)/public:/usr/src/app/public -d -p 8000:8000 musit/frontend
+docker run --name musit-frontend -v $(pwd)/src:/usr/src/app/src -v $(pwd)/public:/usr/src/app/public -d -p 8000:8000 musit/frontend
