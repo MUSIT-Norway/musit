59cbc010716fe45b2dd9288af029f3b64d77c908
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-06-21 14:05:24 +0200

    Musark 138 (#64)
    * Continue changing the MusitTextfields to Musitfield
    
    * Changed the EnvironmentRequirementComponent fields.
    
    * Formatting: done
    
    * Updated MusitArea component. Added tooltip, validation and lines.
    
    * Simplified and corrected the validation code.
    
    * Added the test case for EnvironmentRequirementComponent

--- frontend/src/components/formfields/musitfield/index.js
+++ frontend/src/components/formfields/musitfield/index.js
@@ -1,22 +1,30 @@
+/*
+ *  MUSIT is a museum database to archive natural and cultural history data.
+ *  Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License,
+ *  or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 import React, { Component, PropTypes } from 'react';
-import { Popover } from 'react-bootstrap'
 import { validate } from '../common/validators'
 
 export default class MusitField extends Component {
 
-  static helpText(tip) {
-    return (
-      <Popover id="InputLinkPopover" title="Info">
-        {tip}
-      </Popover>
-    )
-  }
-
   classNameWithSpan() {
     let lvString = ' '
-    if (validate(this, this.props.value, this.props.validate,
-      { minimumLength: this.props.minimumLength, maximumLength: this.props.maximumLength }) === 'error') {
+    if (validate(this.props) === 'error') {
       lvString = 'input-group has-error'
     } else {
       lvString = 'input-group'
@@ -26,15 +34,13 @@ export default class MusitField extends Component {
 
   classNameOnlyWithInput() {
     let lvString = ''
-    if (validate(this, this.props.value, this.props.validate,
-      this.props.minimumLength, { maximumLength: this.props.maximumLength }) === 'error') {
+    if (validate(this.props) === 'error') {
       lvString = 'has-error'
     } else {
       lvString = ''
     }
     return lvString
   }
-
 
   render() {
     const lcAddOnPrefix = this.props.addOnPrefix ? <span className="input-group-addon" >{this.props.addOnPrefix}</span> : null;
@@ -72,5 +78,5 @@ MusitField.propTypes = {
   validate: PropTypes.string.isRequired,
   minimumLength: PropTypes.number,
   maximumLength: PropTypes.number,
-  validator: PropTypes.func
+  validator: PropTypes.string
 };
