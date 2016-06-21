280f088b2d2f856dda08a1f64382228e21bb0ff7
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-06-21 23:05:59 +0200

    Adding docker-compose

--- frontend/docker.sh
+++
@@ -1,5 +0,0 @@
-cp ../fake_security.json .
-docker stop musit-frontend
-docker rm musit-frontend
-docker build -t musit/frontend .
-docker run --name musit-frontend -v $(pwd)/src:/usr/src/app/src -v $(pwd)/public:/usr/src/app/public -d -p 8000:8000 musit/frontend
