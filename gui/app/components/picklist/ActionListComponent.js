0bf9944f8ab7188fecf7232ea54bd8a5c993fe80
Klaus Myrseth <klaus.myrseth@acando.no>
2016-06-21 14:45:02 +0200

    Added adjustments after interaction designer talk

---
+++ gui/app/components/picklist/ActionListComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Well } from 'react-bootstrap'
+
+export default (actions) => {
+  const style = require('./index.scss')
+  console.log(actions)
+  const actionList = actions.map((action) => (
+    <Button key={action.id} onClick={action.func} block>{action.description}</Button>
+  ))
+  return (
+    <Well className={style.actionList}>
+      { actionList }
+    </Well>
+  )
+}
--- gui/app/containers/picklist/PickListContainer.js
+++ gui/app/containers/picklist/PickListContainer.js
@@ -11,9 +11,39 @@ export default class PickListContainer extends React.Component {
     onToggleMarked: React.PropTypes.func.isRequired
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      showActionDialog: false
+    }
+    this.onOpenActionDialog = this.onOpenActionDialog.bind(this)
+    this.onCloseActionDialog = this.onCloseActionDialog.bind(this)
+  }
+
+  onOpenActionDialog() {
+    this.setState({ showActionDialog: true })
+  }
+
+  onCloseActionDialog() {
+    this.setState({ showActionDialog: false })
+  }
+
   render() {
+    const demoActions = [
+      {
+        id: 'testAction',
+        description: 'Demo Action',
+        func: () => alert('works')
+      },
+      {
+        id: 'testAction2',
+        description: 'Demo Action 2',
+        func: () => alert('works 2')
+      }
+    ]
     const style = require('../../components/picklist/index.scss')
     const { translate, picks, marked, onToggleMarked } = this.props
+    const { showActionDialog } = this.state
     const renderIcon = () => {return (<FontAwesome name="archive" />)}
     const renderLabel = (pick) => {return pick.name}
     const checkSymbol = (marked.length > 0) ? 'square-o' : 'check-square-o'
@@ -28,7 +58,7 @@ export default class PickListContainer extends React.Component {
                 <FontAwesome name={checkSymbol} className={style.normalAction} />
                 {translate('musit.pickList.action.markAll', false)}
               </Button>
-              <Button>
+              <Button disabled={marked.length <= 0} onClick={() => this.onOpenActionDialog()}>
                 <FontAwesome name="play-circle" className={style.normalAction} />
                 {translate('musit.pickList.action.executeAll', false)}
               </Button>
@@ -36,10 +66,16 @@ export default class PickListContainer extends React.Component {
             <PickListComponent
               picks={picks}
               marked={marked}
+              actions={demoActions}
+              showActionDialog={showActionDialog}
+              onCloseActionDialog={() => this.onCloseActionDialog()}
               iconRendrer={renderIcon}
               labelRendrer={renderLabel}
               onToggleMarked={onToggleMarked}
             />
+          <div style={{ textAlign: 'right' }}>
+            {marked.length}/{picks.length} node(r) valgt.
+          </div>
           </Panel>
         </main>
       </div>
