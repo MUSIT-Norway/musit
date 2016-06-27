6e00a11c332a99ac7c705080da547d7bf9f4b017
Stein Olsen <stein.olsen@usit.uio.no>
2016-06-27 16:17:37 +0200

    Started on new component for paired toggle button

---
+++ frontend/src/components/controls/pairedToggleButtons.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class pairedToogleButtons extends React.Component {
+  static PropTypes = {
+    label: React.PropTypes.string.isRequired,
+    value: React.PropTypes.string
+  }
+}
---
+++ frontend/src/reducers/control/index.js
@@ -0,0 +1,44 @@
+const ADD = 'musit/control/ADD'
+const ADD_SUCCESS = 'musit/control/ADD_SUCCESS'
+const ADD_FAILURE = 'musit/control/ADD_FAILURE'
+
+const initialState = {
+  user: '',
+  date: '',
+  data: {
+    temperatureOK: '',
+    inertAirOK: '',
+    cleaningOK: '',
+    relativeHumidity: '',
+    lightConditionsOK: '',
+    alchoholOK: '',
+    pestOK: '',
+    moldFungusOK: ''
+  }
+}
+
+const controlReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case ADD: return {
+      ...state,
+      data: action.data,
+      loading: true,
+      loaded: false
+    }
+    case ADD_SUCCESS: return {
+      ...state,
+      data: action.data,
+      loading: false,
+      loaded: true
+    }
+    case ADD_FAILURE: return {
+      ...state,
+      loading: false,
+      loaded: false
+    }
+    default:
+      return state
+  }
+}
+
+export default controlReducer
