eacbc35cd5deed6de9c233dfcd667cca7bb2c7af
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-07-25 12:50:08 +0200

    Musark 183 (#84)
    * Added folder structure for grid issues. Started with 183
    
    * Continue working. Try to fix 191 conflict so save
    
    * Added static data.
    
    * Merge from master.
    
    * Added logic to read data from reducer and added id's on all the components.
    
    * Working on test case.
    
    * Added language support.
    
    * Added testcases.
    
    * Tested and added test cases.
    
    * Implemented the review comments.

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -31,6 +31,8 @@ import organizationReducer from './organization'
 import observationReducer from './observation'
 import controlAddReducer from './control/add'
 import controlDetailsReducer from './control/view'
+import objectGridReducer from './grid/object'
+
 
 const rootReducer = combineReducers({
   routing: routerReducer,
@@ -45,7 +47,8 @@ const rootReducer = combineReducers({
   organization: organizationReducer,
   observation: observationReducer,
   control: controlAddReducer,
-  controlDetails: controlDetailsReducer
+  controlDetails: controlDetailsReducer,
+  objectGrid: objectGridReducer
 })
 
 export default rootReducer
