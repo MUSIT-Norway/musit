59d3074c266a9d59079e11b9935029bf27b9b1db
Rituvesh Kumar <rituvesh.kumar@usit.uio.no>
2016-08-11 12:48:52 +0200

    Musark 171 (#105)
    * Created common component for save and cancel buttons. Used the component on Add control.
    
    * Added newURL and Open the URL from register observation button.
    
    * Continue updating the observations screen.
    
    * Added control observations.
    
    * Fixed the review comments.

--- frontend/src/components/formfields/index.js
+++ frontend/src/components/formfields/index.js
@@ -2,5 +2,6 @@ import MusitDropDownField from './musitdropdownfield'
 import MusitField from './musitfield'
 import MusitTextArea from './musittextarea'
 import MusitTextField from './musittextfield'
+import SaveCancel from './saveCancel/SaveCancel'
 
-export { MusitDropDownField, MusitField, MusitTextArea, MusitTextField }
+export { MusitDropDownField, MusitField, MusitTextArea, MusitTextField, SaveCancel }
