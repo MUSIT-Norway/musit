b92cb77a16e32b9a59a05a0f660cb0e5133d538d
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-09-13 08:26:25 +0200

    adding musitresults + examples for slick

--- service_thing_aggregate/src/main/scala/dao/ObjectAggregationDao.scala
+++ service_thing_aggregate/src/main/scala/dao/ObjectAggregationDao.scala
@@ -1,20 +1,67 @@
 package dao
 
 import com.google.inject.Inject
-import models.{ NodeId, ObjectAggregation, ObjectId }
-import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
+import models.{NodeId, ObjectAggregation, ObjectId}
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.driver.JdbcProfile
+import slick.jdbc.GetResult
 
 import scala.concurrent.Future
 
 class ObjectAggregationDao @Inject() (
     val dbConfigProvider: DatabaseConfigProvider
 ) extends HasDatabaseConfigProvider[JdbcProfile] {
+  import driver.api._
 
   def getOjects(nodeId: Long): Future[Seq[ObjectAggregation]] = {
+
     Future.successful(Seq(
       ObjectAggregation(ObjectId(1), "Test", NodeId(1))
     ))
   }
+
+  def createCoffees: DBIO[Int] =
+    sqlu"""create table coffees(
+    name varchar not null,
+    sup_id int not null,
+    price double not null,
+    sales int not null,
+    total int not null,
+    foreign key(sup_id) references suppliers(id))"""
+
+  def createSuppliers: DBIO[Int] =
+    sqlu"""create table suppliers(
+    id int not null primary key,
+    name varchar not null,
+    street varchar not null,
+    city varchar not null,
+    state varchar not null,
+    zip varchar not null)"""
+
+  def insertSuppliers: DBIO[Unit] = DBIO.seq(
+    // Insert some suppliers
+    sqlu"insert into suppliers values(101, 'Acme, Inc.', '99 Market Street', 'Groundsville', 'CA', '95199')",
+    sqlu"insert into suppliers values(49, 'Superior Coffee', '1 Party Place', 'Mendocino', 'CA', '95460')",
+    sqlu"insert into suppliers values(150, 'The High Ground', '100 Coffee Lane', 'Meadows', 'CA', '93966')"
+  )
+
+  // Case classes for our data
+  case class Supplier(id: Int, name: String, street: String, city: String, state: String, zip: String)
+  case class Coffee(name: String, supID: Int, price: Double, sales: Int, total: Int)
+
+  // Result set getters
+  implicit val getSupplierResult = GetResult(r => Supplier(r.nextInt, r.nextString, r.nextString,
+    r.nextString, r.nextString, r.nextString))
+  implicit val getCoffeeResult = GetResult(r => Coffee(r.<<, r.<<, r.<<, r.<<, r.<<))
+
+  val price = 1
+
+  val test = sql"""select c.name, s.name
+      from coffees c, suppliers s
+      where c.price < $price and s.id = c.sup_id""".as[(String, String)]
+
+  val name = "Starbucks"
+  val table = "coffees"
+  sql"select * from #$table where name = $name".as[Coffee].headOption
 
 }
