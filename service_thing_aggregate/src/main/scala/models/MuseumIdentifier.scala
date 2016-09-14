cd758cff6a7b475337f32e252eab364c7c795979
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2016-09-14 11:44:10 +0200

    Musark 326 (#131)
    * Initial commit
    
    * Sculpting out the basic skeleton for the service
    
    * Gluing the skeleton together
    
    * Adjusting a little bit and creating a dummy integration spec
    
    * Adding test folder + fixing a scalastyle warning
    
    * add more to case class
    
    * WIP
    
    * adding musitresults + examples for slick
    
    * got the sql in a comment
    
    * Ready for review
    
    * Moving evolutions file to main resources
    
    * adding to docker + adding settings for application
    
    * Cleaning up
    
    * ready for pr
    
    * Adding timeout configuration
    
    * Fixing shared code between test and it:test
    
    * Fixing PR comments
    
    * Fixing scalastyle warning

---
+++ service_thing_aggregate/src/main/scala/models/MuseumIdentifier.scala
@@ -0,0 +1,18 @@
+package models
+
+import play.api.libs.json.Json
+
+case class MuseumIdentifier(museumNo: String, subNo: Option[String])
+
+object MuseumIdentifier {
+  def fromSqlString(displayId: String): MuseumIdentifier =
+    displayId.split("/") match {
+      case Array(museumNo, subNo) =>
+        MuseumIdentifier(museumNo, Some(subNo))
+      case Array(museumNo) =>
+        MuseumIdentifier(museumNo, None)
+    }
+
+  implicit val format = Json.format[MuseumIdentifier]
+
+}
