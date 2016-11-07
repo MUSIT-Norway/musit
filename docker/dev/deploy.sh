b7065b17b059a11516effc2a0f3e48393eb00b10
Jarl André Hübenthal <jarl.andre@gmail.com>
2016-11-07 10:50:56 +0100

    Removing password and app secret from docker compose files

--- docker/dev/deploy.sh
+++ docker/dev/deploy.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-export EVOLUTION_ENABLED=false
+echo "################################################################################"
+echo "## IMPORTANT!"
+echo "## This script is ONLY meant for DEVELOPMENT, and NOT for PRODUCTION."
+echo "## If you are seeing this message in PRODUCTION, something has gone terribly wrong!"
+echo "################################################################################"
 
+export EVOLUTION_ENABLED=false
+export APPLICATION_SECRET=dummyAppSecret
 export SLICK_DRIVER=slick.driver.PostgresDriver$
 export SLICK_DB_DRIVER=org.postgresql.Driver
 export SLICK_DB_URL=jdbc:postgresql://db/postgres
 export SLICK_DB_USER=postgres
 export SLICK_DB_PASSWORD=postgres
-
-export MILJO=dev
 
 STARTDIR=$(pwd)
 
