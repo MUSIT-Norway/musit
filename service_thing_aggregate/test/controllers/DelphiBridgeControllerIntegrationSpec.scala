3089b6fd348225178fe18dfe688a9de8baaa399f
Knut Petter Meen <knutpm@usit.uio.no>
2016-12-06 20:47:55 +0100

    Unused import

--- service_thing_aggregate/test/controllers/DelphiBridgeControllerIntegrationSpec.scala
+++ service_thing_aggregate/test/controllers/DelphiBridgeControllerIntegrationSpec.scala
@@ -23,7 +23,6 @@ import no.uio.musit.security.BearerToken
 import no.uio.musit.security.fake.FakeAuthenticator
 import no.uio.musit.test.MusitSpecWithServerPerSuite
 import org.scalatest.time.{Millis, Seconds, Span}
-import play.api.libs.json._
 import play.api.test.Helpers._
 
 class DelphiBridgeControllerIntegrationSpec extends MusitSpecWithServerPerSuite {
