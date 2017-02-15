b45f35af160c42ea78ae24a65b42ca0725ee6f73
Ingar Abrahamsen <ingar@abrahams1.com>
2017-02-15 13:42:46 +0100

    add health check service
    basic health check service that can be wired up in the play project.

---
+++ musit-service/src/main/scala/no/uio/musit/healthcheck/HealthCheckDao.scala
@@ -0,0 +1,70 @@
+/*
+ *  MUSIT is a museum database to archive natural and cultural history data.
+ *  Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License,
+ *  or any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package no.uio.musit.healthcheck
+
+import com.google.inject.{Inject, Singleton}
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.driver.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class HealthCheckDao @Inject() (
+    val dbConfigProvider: DatabaseConfigProvider
+)(
+    implicit
+    ec: ExecutionContext
+) extends HasDatabaseConfigProvider[JdbcProfile] with HealthCheck {
+
+  import driver.api._
+
+  val CheckName = "Database"
+
+  override def healthCheck(): Future[HealthCheckStatus] = {
+    val checkConnectionQuery = sql"""select 1 from dual""".as[Int]
+    val stopWatch = StopWatch()
+
+    db.run(checkConnectionQuery).map {
+      case Vector(1) =>
+        HealthCheckStatus(
+          CheckName,
+          available = true,
+          responseTime = stopWatch.elapsed(),
+          None
+        )
+      case r =>
+        HealthCheckStatus(
+          CheckName,
+          available = false,
+          responseTime = stopWatch.elapsed(),
+          Some(s"Unexpected result from health check query. Result: $r")
+        )
+    }.recover {
+      case e: Exception =>
+        HealthCheckStatus(
+          CheckName,
+          available = false,
+          responseTime = stopWatch.elapsed(),
+          Some(s"Unexpected result from health check query. Reason: ${e.getMessage}")
+        )
+    }
+  }
+
+}
