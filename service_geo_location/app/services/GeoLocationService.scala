04749cd9a1eaefb6d8d2e5165c0a874b5972eb40
Ingar Abrahamsen <ingar@abrahams1.com>
2017-03-22 14:25:52 +0100

    MUSARK-944 - Feature/configurable endpoint urls (#274)
    * feat: use geo norway url from config before defaulting to url
    
    In production we need to call the service trough a proxy.
    
    * feat: make dataporten urls configurable trough environment
    
    In production we need to call the endpoints trough a proxy.
    
    * refactor: move url to config
    
    * feat: use ficus configuration
    
    * feat: proxy patcher for ws client
    
    This is a easy way to patch the client with a proxy server
    without concerning about the configuration.
    
    * feat: add proxy to geo norway and dataporten requests
    
    * refactor: use pattern matching directly
    
    Fix for codacy issue
    
    * refactor: improve usage of the viaProxy call.
    
    Changes reflects the feedback from the code review.
    - Remove parentheses on viaProxy since it doesn't have side effects
    - Remove implicit signature on via proxy function
    - Create a singleton instance of ViaProxy to remove usage of trait.

--- service_geo_location/app/services/GeoLocationService.scala
+++ service_geo_location/app/services/GeoLocationService.scala
@@ -1,45 +1,33 @@
-/*
- * MUSIT is a museum database to archive natural and cultural history data.
- * Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License,
- * or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
 package services
 
 import com.google.inject.Inject
 import models.{Address, GeoNorwayAddress}
-import play.api.{Configuration, Logger}
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import no.uio.musit.ws.ViaProxy.viaProxy
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
 import play.api.libs.ws.WSClient
-import GeoLocationService._
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.{Configuration, Logger}
 
 import scala.concurrent.Future
 
-class GeoLocationService @Inject()(config: Configuration, ws: WSClient) {
+case class GeoLocationConfig(url: String, hitsPerResult: Int)
+
+class GeoLocationService @Inject()(ws: WSClient)(implicit config: Configuration) {
 
   val logger = Logger(classOf[GeoLocationService])
 
-  def searchGeoNorway(expr: String): Future[Seq[Address]] = {
-    val maxRes = config.getInt(HitsPerResultKey).getOrElse(10)
+  val geoLocationConfig =
+    config.underlying.as[GeoLocationConfig]("musit.geoLocation.geoNorway")
 
-    ws.url(SearchURL)
+  def searchGeoNorway(expr: String): Future[Seq[Address]] = {
+
+    ws.url(geoLocationConfig.url)
+      .viaProxy
       .withQueryString(
         "sokestreng" -> expr,
-        "antPerSide" -> s"$maxRes"
+        "antPerSide" -> s"${geoLocationConfig.hitsPerResult}"
       )
       .get()
       .map { response =>
@@ -64,11 +52,5 @@ class GeoLocationService @Inject()(config: Configuration, ws: WSClient) {
         }
       }
   }
-
-}
-
-object GeoLocationService {
-  val HitsPerResultKey = "musit.geoLocation.geoNorway.hitsPerResult"
-  val SearchURL        = "http://ws.geonorge.no/AdresseWS/adresse/sok"
 
 }
