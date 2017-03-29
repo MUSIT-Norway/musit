752b89548a7aaffc36227c94c552ffa7a084d261
Ingar Abrahamsen <ingar@abrahams1.com>
2017-03-29 12:25:34 +0200

    test: add test for qr code

--- service_barcode/test/services/GeneratorSpec.scala
+++ service_barcode/test/services/GeneratorSpec.scala
@@ -10,8 +10,9 @@ import akka.stream.scaladsl.StreamConverters
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import com.google.zxing.datamatrix.DataMatrixReader
+import com.google.zxing.qrcode.QRCodeReader
 import com.google.zxing.{BinaryBitmap, DecodeHintType}
-import models.BarcodeFormats.DataMatrix
+import models.BarcodeFormats.{DataMatrix, QrCode}
 import org.scalatest.{MustMatchers, OptionValues, WordSpec}
 
 import scala.collection.JavaConverters._
@@ -22,8 +23,7 @@ class GeneratorSpec extends WordSpec with OptionValues with MustMatchers {
     implicit val as  = ActorSystem("test")
     implicit val mat = ActorMaterializer.create(as)
 
-    val reader = new DataMatrixReader
-    val hints  = Map(DecodeHintType.PURE_BARCODE -> true).asJava
+    val hints = Map(DecodeHintType.PURE_BARCODE -> true).asJava
 
     "generate and parse UUID from Data Matrix code" in {
       val content = UUID.randomUUID()
@@ -35,7 +35,22 @@ class GeneratorSpec extends WordSpec with OptionValues with MustMatchers {
         .map(streamToBitmap)
         .value
 
-      val res = reader.decode(image, hints)
+      val res = (new DataMatrixReader).decode(image, hints)
+
+      res.getText mustBe content.toString
+    }
+
+    "generate and parse UUID from QR code" in {
+      val content = UUID.randomUUID()
+
+      val image = Generator
+        .generatorFor(QrCode)
+        .flatMap(_.write(content))
+        .map(_.runWith(StreamConverters.asInputStream()))
+        .map(streamToBitmap)
+        .value
+
+      val res = (new QRCodeReader).decode(image, hints)
 
       res.getText mustBe content.toString
     }
