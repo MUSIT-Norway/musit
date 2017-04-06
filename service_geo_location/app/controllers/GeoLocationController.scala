d5a80a8557280fc7b2113124c5312d2dd0e0d22e
Ellen <ellen.jordal@usit.uio.no>
2017-04-06 12:37:22 +0200

    fix: return 503 when service is not available in service_geo_location

--- service_geo_location/app/controllers/GeoLocationController.scala
+++ service_geo_location/app/controllers/GeoLocationController.scala
@@ -20,6 +20,7 @@
 package controllers
 
 import com.google.inject.Inject
+import no.uio.musit.MusitResults.{MusitError, MusitHttpError, MusitSuccess}
 import no.uio.musit.security.Authenticator
 import no.uio.musit.service.MusitController
 import play.api.Logger
@@ -46,8 +47,19 @@ class GeoLocationController @Inject()(
     val expression = search.getOrElse("")
     geoLocService
       .searchGeoNorway(expression)
-      .map { location =>
-        Ok(Json.toJson(location))
+      .map {
+        case MusitSuccess(locations) => {
+          Ok(Json.toJson(locations))
+        }
+
+        case MusitHttpError(status, msg) => {
+          println("inni map. MusitHttpError")
+          Status(status)
+        }
+
+        case err: MusitError =>
+          logger.error("InternalServerError: " + s"${err.message}")
+          InternalServerError(Json.obj("message" -> err.message))
       }
       .recover {
         case NonFatal(ex) =>
