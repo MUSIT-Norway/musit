51910ecd6487eeaf5ff8cef6fa99148aed850392
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-06 17:22:50 +0200

    fix: use max size of matrix and preferred image (#292)
    The size hints to the data matrix generator is ignored
    so we have to use the actual size from the matrix it
    self. the issue was that the image was cut and made
    invalid/unable to scan.
    
    Fixes MUSARK-994

--- service_barcode/test/services/GeneratorSpec.scala
+++ service_barcode/test/services/GeneratorSpec.scala
@@ -19,14 +19,28 @@ import scala.collection.JavaConverters._
 
 class GeneratorSpec extends WordSpec with OptionValues with MustMatchers {
 
-  "Generator" should {
-    implicit val as  = ActorSystem("test")
-    implicit val mat = ActorMaterializer.create(as)
+  implicit val as  = ActorSystem("test")
+  implicit val mat = ActorMaterializer.create(as)
 
-    val hints = Map(DecodeHintType.PURE_BARCODE -> true).asJava
+  val hints = Map(DecodeHintType.PURE_BARCODE -> true).asJava
 
-    "generate and parse UUID from Data Matrix code" in {
+  "Generator DataMatrix" should {
+    "generate and parse UUID" in {
       val content = UUID.randomUUID()
+      val image = Generator
+        .generatorFor(DataMatrix)
+        .flatMap(_.write(content))
+        .map(_.runWith(StreamConverters.asInputStream()))
+        .map(streamToBitmap)
+        .value
+
+      val res = (new DataMatrixReader).decode(image, hints)
+
+      res.getText mustBe content.toString
+    }
+
+    "not cast FormatException" in {
+      val content = UUID.fromString("6c2a5518-c0c6-4b2e-ace3-40a5abd57a15")
 
       val image = Generator
         .generatorFor(DataMatrix)
@@ -40,6 +54,23 @@ class GeneratorSpec extends WordSpec with OptionValues with MustMatchers {
       res.getText mustBe content.toString
     }
 
+    "not cast ChecksumException" in {
+      val content = UUID.fromString("ffb42bb8-fee7-48fe-8eb0-d3f815f45644")
+
+      val image = Generator
+        .generatorFor(DataMatrix)
+        .flatMap(_.write(content))
+        .map(_.runWith(StreamConverters.asInputStream()))
+        .map(streamToBitmap)
+        .value
+
+      val res = (new DataMatrixReader).decode(image, hints)
+
+      res.getText mustBe content.toString
+    }
+  }
+
+  "Generator Qr-Code" should {
     "generate and parse UUID from QR code" in {
       val content = UUID.randomUUID()
 
