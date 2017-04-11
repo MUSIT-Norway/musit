b4d38cf27edfe2c410160918dc0325aad8028bfe
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-11 14:32:04 +0200

    test: ensure not to load test resources that doesn't exists
    It's only the play projects that set the resource files trough sbt.
    Now we're loading the resources if they exists, something they should
    not as long as it's not a play project.

--- musit-test/src/main/scala/no/uio/musit/test/MusitFakeApplication.scala
+++ musit-test/src/main/scala/no/uio/musit/test/MusitFakeApplication.scala
@@ -5,16 +5,19 @@ import play.api.inject.guice.GuiceApplicationBuilder
 trait MusitFakeApplication extends TestConfigs {
 
   def createApplication() = {
-    if (notExecutedFromSbt) {
+    if (notExecutedFromPlayProject) {
       setProperty("config.file", "/application.test.conf")
       setProperty("logger.file", "/logback-test.xml")
     }
     new GuiceApplicationBuilder().configure(slickWithInMemoryH2()).build()
   }
 
-  private def notExecutedFromSbt = System.getProperty("config.file") == null
+  private def notExecutedFromPlayProject =
+    System.getProperty("config.file") == null
 
-  private def setProperty(p: String, r: String): Unit =
-    System.setProperty(p, this.getClass.getResource(r).getPath)
+  private def setProperty(p: String, r: String): Unit = {
+    val resource = this.getClass.getResource(r)
+    if (resource != null) System.setProperty(p, resource.getPath)
+  }
 
 }
