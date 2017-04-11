e634f958e123143acbc96ceb9cc639a1b9ea6d2e
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-11 09:56:38 +0200

    feat: do not accept loans without objects

--- service_management/app/services/loan/LoanService.scala
+++ service_management/app/services/loan/LoanService.scala
@@ -2,7 +2,7 @@ package services.loan
 
 import com.google.inject.Inject
 import models.loan.event.{LoanEvent, ObjectsLent}
-import no.uio.musit.MusitResults.MusitResult
+import no.uio.musit.MusitResults.{MusitResult, MusitValidationError}
 import no.uio.musit.models.{EventId, MuseumId}
 import no.uio.musit.security.AuthenticatedUser
 import no.uio.musit.time.dateTimeNow
@@ -15,11 +15,15 @@ class LoanService @Inject()(loanDao: LoanDao) {
   def createLoan(mid: MuseumId, ol: ObjectsLent)(
       implicit currUser: AuthenticatedUser
   ): Future[MusitResult[EventId]] = {
-    val toInsert = ol.copy(
-      registeredBy = Some(currUser.id),
-      registeredDate = Some(dateTimeNow)
-    )
-    loanDao.insertLentObjectEvent(mid, toInsert)
+    if (ol.objects.isEmpty) {
+      Future.successful(MusitValidationError("Can't create a loan without any objects"))
+    } else {
+      val toInsert = ol.copy(
+        registeredBy = Some(currUser.id),
+        registeredDate = Some(dateTimeNow)
+      )
+      loanDao.insertLentObjectEvent(mid, toInsert)
+    }
   }
 
   def findActiveLoans(mid: MuseumId): Future[MusitResult[Seq[LoanEvent]]] =
--- service_management/test/controllers/loan/LoanControllerSpec.scala
+++ service_management/test/controllers/loan/LoanControllerSpec.scala
@@ -1,10 +1,11 @@
 package controllers.loan
 
-import no.uio.musit.models.MuseumId
+import no.uio.musit.models.{MuseumId, ObjectUUID}
 import no.uio.musit.security.BearerToken
 import no.uio.musit.test.{FakeUsers, MusitSpecWithServerPerSuite}
 import no.uio.musit.time._
 import play.api.libs.json.{JsString, Json}
+import play.mvc.Http.Status
 import play.mvc.Http.Status.CREATED
 
 class LoanControllerSpec extends MusitSpecWithServerPerSuite {
@@ -17,17 +18,32 @@ class LoanControllerSpec extends MusitSpecWithServerPerSuite {
   val activeLoanUrl = (mid: Int) => s"/$mid/loans/active"
 
   "Using the loan controller" when {
-    "create a new loan" in {
-      val js = Json.obj(
-        "externalRef" -> Json.arr("ref-1", "ref-2"),
-        "note"        -> "a note",
-        "returnDate"  -> JsString(dateTimeNow.plusMonths(1).toString("yyyy-MM-dd")),
-        "objects"     -> Json.arr()
-      )
-      val res =
-        wsUrl(createLoanUrl(mid)).withHeaders(token.asHeader).post(js).futureValue
+    "working with loan" should {
+      "create a new loan" in {
+        val js = Json.obj(
+          "externalRef" -> Json.arr("ref-1", "ref-2"),
+          "note"        -> "a note",
+          "returnDate"  -> JsString(dateTimeNow.plusMonths(1).toString("yyyy-MM-dd")),
+          "objects"     -> Json.arr(ObjectUUID.generate())
+        )
+        val res =
+          wsUrl(createLoanUrl(mid)).withHeaders(token.asHeader).post(js).futureValue
 
-      res.status mustBe CREATED
+        res.status mustBe CREATED
+      }
+
+      "not create new loans without any objects" in {
+        val js = Json.obj(
+          "externalRef" -> Json.arr("ref-1", "ref-2"),
+          "note"        -> "a note",
+          "returnDate"  -> JsString(dateTimeNow.plusMonths(1).toString("yyyy-MM-dd")),
+          "objects"     -> Json.arr()
+        )
+        val res =
+          wsUrl(createLoanUrl(mid)).withHeaders(token.asHeader).post(js).futureValue
+
+        res.status mustBe Status.BAD_REQUEST
+      }
     }
   }
 }
