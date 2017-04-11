92f3be392cefc45463b75cca8fd89e982007533a
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-11 14:00:48 +0200

    test: load test resource files from classpath
    Intellij Idea doesn't read the sbt config when running tests
    from the editor. All tests that's not depending on configuration
    loaded from the application.test.conf does work without any
    modification. This is a workaround that enables us to run all the tests
    trough Idea in a way that doesn't conflict with how sbt setup the test
    environment.

--- musit-test/src/main/scala/no/uio/musit/test/MusitFakeApplication.scala
+++ musit-test/src/main/scala/no/uio/musit/test/MusitFakeApplication.scala
@@ -4,7 +4,17 @@ import play.api.inject.guice.GuiceApplicationBuilder
 
 trait MusitFakeApplication extends TestConfigs {
 
-  def createApplication() =
+  def createApplication() = {
+    if (notExecutedFromSbt) {
+      setProperty("config.file", "/application.test.conf")
+      setProperty("logger.file", "/logback-test.xml")
+    }
     new GuiceApplicationBuilder().configure(slickWithInMemoryH2()).build()
+  }
+
+  private def notExecutedFromSbt = System.getProperty("config.file") == null
+
+  private def setProperty(p: String, r: String): Unit =
+    System.setProperty(p, this.getClass.getResource(r).getPath)
 
 }
