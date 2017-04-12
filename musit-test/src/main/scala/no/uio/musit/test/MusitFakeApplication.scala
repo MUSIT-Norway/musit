862ee2df9670934315a1414466ac776a8e7e989c
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-12 12:03:50 +0200

    Run tests from intellij (#294)
    * chore: remove copyright header
    
    * test: load test resource files from classpath
    
    Intellij Idea doesn't read the sbt config when running tests
    from the editor. All tests that's not depending on configuration
    loaded from the application.test.conf does work without any
    modification. This is a workaround that enables us to run all the tests
    trough Idea in a way that doesn't conflict with how sbt setup the test
    environment.
    
    * test: ensure not to load test resources that doesn't exists
    
    It's only the play projects that set the resource files trough sbt.
    Now we're loading the resources if they exists, something they should
    not as long as it's not a play project.
    
    * test: refactor to not use java api

--- musit-test/src/main/scala/no/uio/musit/test/MusitFakeApplication.scala
+++ musit-test/src/main/scala/no/uio/musit/test/MusitFakeApplication.scala
@@ -1,29 +1,22 @@
-/*
- * MUSIT is a museum database to archive natural and cultural history data.
- * Copyright (C) 2016  MUSIT Norway, part of www.uio.no (University of Oslo)
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License,
- * or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
 package no.uio.musit.test
 
 import play.api.inject.guice.GuiceApplicationBuilder
 
 trait MusitFakeApplication extends TestConfigs {
 
-  def createApplication() =
+  def createApplication() = {
+    if (notExecutedFromPlayProject) {
+      setPropertyIfFileExists("config.file", "/application.test.conf")
+      setPropertyIfFileExists("logger.file", "/logback-test.xml")
+    }
     new GuiceApplicationBuilder().configure(slickWithInMemoryH2()).build()
+  }
+
+  private def notExecutedFromPlayProject =
+    sys.props.get("config.file").isEmpty
+
+  private def setPropertyIfFileExists(p: String, r: String): Unit =
+    Option(this.getClass.getResource(r))
+      .foreach(res => System.setProperty(p, res.getPath))
 
 }
