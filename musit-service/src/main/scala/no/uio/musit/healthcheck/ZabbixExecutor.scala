c9665ea3e6d95a59cdd7f29f7f3606aa83d4f4e7
Ingar Abrahamsen <ingar@abrahams1.com>
2017-04-21 13:49:08 +0200

    fix: write over all the content in the file if it exists
    When the content we want to write is shorter than the content in
    the existing file we're ending up with some unwanted content from
    the current file after we have written the new content. This ensure
    that we clear the file before we start writing new content to it.
    
    Resolves MUSARK-1001

--- musit-service/src/main/scala/no/uio/musit/healthcheck/ZabbixExecutor.scala
+++ musit-service/src/main/scala/no/uio/musit/healthcheck/ZabbixExecutor.scala
@@ -20,6 +20,7 @@
 package no.uio.musit.healthcheck
 
 import java.io.File
+import java.nio.file.StandardOpenOption.{CREATE, TRUNCATE_EXISTING, WRITE}
 import java.nio.file.Files
 
 import akka.actor.ActorSystem
@@ -80,7 +81,12 @@ class ZabbixExecutor(
   private def writeToFile(z: Zabbix): Future[IOResult] = {
     val sink = Flow[String]
       .map(s => ByteString(s))
-      .toMat(FileIO.toPath(zabbaxFile.ensureWritableFile().toPath))(Keep.right)
+      .toMat(
+        FileIO.toPath(
+          zabbaxFile.ensureWritableFile().toPath,
+          Set(CREATE, WRITE, TRUNCATE_EXISTING)
+        )
+      )(Keep.right)
 
     Source.fromFuture(Future.successful(Json.prettyPrint(z.toJson))).runWith(sink)
   }
