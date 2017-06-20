d18e1e9538771d59ea3f139876fb9f534bc39096
Knut Petter Meen <kp@scalytica.net>
2017-06-20 09:00:43 +0000

    Increasing timeout values from 20000 ms => 30000 ms in an attempt to please the CI server demons.

--- musit-test/src/main/scala/no/uio/musit/test/TestConfigs.scala
+++ musit-test/src/main/scala/no/uio/musit/test/TestConfigs.scala
@@ -11,9 +11,9 @@ trait TestConfigs {
     "play.evolutions.db.default.autoApply"  -> false,
     "play.slick.db.default"                 -> "test",
     "slick.dbs.test.driver"                 -> "slick.driver.H2Driver$",
-    "slick.dbs.test.connectionTimeout"      -> "20000",
-    "slick.dbs.test.loginTimeout"           -> "20000",
-    "slick.dbs.test.socketTimeout"          -> "20000",
+    "slick.dbs.test.connectionTimeout"      -> "30000",
+    "slick.dbs.test.loginTimeout"           -> "30000",
+    "slick.dbs.test.socketTimeout"          -> "30000",
     "slick.dbs.test.db.driver"              -> "org.h2.Driver",
     "slick.dbs.test.connectionTestQuery"    -> "SELECT 1",
     "slick.dbs.test.db.url"                 -> s"jdbc:h2:mem:musit-test${Random.nextInt()};MODE=Oracle;DB_CLOSE_DELAY=-1", // scalastyle:ignore
