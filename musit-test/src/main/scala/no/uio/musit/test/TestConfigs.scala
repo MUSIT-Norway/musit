77bdc61de48c4e61a2982000c35f76b78cbd8c16
Knut Petter Meen <kp@scalytica.net>
2017-06-20 10:25:34 +0000

    Correct DB config for test. _Should_ stop tests failing.

--- musit-test/src/main/scala/no/uio/musit/test/TestConfigs.scala
+++ musit-test/src/main/scala/no/uio/musit/test/TestConfigs.scala
@@ -7,18 +7,18 @@ trait TestConfigs {
   def slickWithInMemoryH2(
       evolve: String = "enabled"
   ): Map[String, Any] = Map(
-    "play.evolutions.db.default.enabled"    -> false,
-    "play.evolutions.db.default.autoApply"  -> false,
-    "play.slick.db.default"                 -> "test",
-    "slick.dbs.test.driver"                 -> "slick.driver.H2Driver$",
-    "slick.dbs.test.connectionTimeout"      -> "30000",
-    "slick.dbs.test.loginTimeout"           -> "30000",
-    "slick.dbs.test.socketTimeout"          -> "30000",
-    "slick.dbs.test.db.driver"              -> "org.h2.Driver",
-    "slick.dbs.test.connectionTestQuery"    -> "SELECT 1",
-    "slick.dbs.test.db.url"                 -> s"jdbc:h2:mem:musit-test${Random.nextInt()};MODE=Oracle;DB_CLOSE_DELAY=-1", // scalastyle:ignore
-    "slick.dbs.test.leakDetectionThreshold" -> "5000",
-    "evolutionplugin"                       -> evolve
+    "play.evolutions.db.default.enabled"       -> false,
+    "play.evolutions.db.default.autoApply"     -> false,
+    "play.slick.db.default"                    -> "test",
+    "slick.dbs.test.driver"                    -> "slick.driver.H2Driver$",
+    "slick.dbs.test.db.connectionTimeout"      -> "20000",
+    "slick.dbs.test.db.loginTimeout"           -> "20000",
+    "slick.dbs.test.db.socketTimeout"          -> "20000",
+    "slick.dbs.test.db.driver"                 -> "org.h2.Driver",
+    "slick.dbs.test.db.connectionTestQuery"    -> "SELECT 1",
+    "slick.dbs.test.db.url"                    -> s"jdbc:h2:mem:musit-test${Random.nextInt()};MODE=Oracle;DB_CLOSE_DELAY=-1", // scalastyle:ignore
+    "slick.dbs.test.db.leakDetectionThreshold" -> "5000",
+    "evolutionplugin"                          -> evolve
   )
 
 }
