bd2223ca1dbc7bdb1a4368953d9490f2564458e8
Ingar Abrahamsen <ingar@abrahams1.com>
2017-08-14 08:50:09 +0200

    refactor: remove proxy setup
    We don't need it since we're in the right zone.

--- service_backend/app/modules/ElasticsearchModule.scala
+++ service_backend/app/modules/ElasticsearchModule.scala
@@ -3,10 +3,6 @@ package modules
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
 import net.codingwell.scalaguice.ScalaModule
-import no.uio.musit.ws.ProxiedRequest
-import org.apache.http.HttpHost
-import org.apache.http.client.config.RequestConfig
-import org.elasticsearch.client.RestClientBuilder.RequestConfigCallback
 import play.api.{Configuration, Environment}
 import services.elasticsearch.ElasticsearchService
 
@@ -19,30 +15,7 @@ class ElasticsearchModule(environment: Environment, cfg: Configuration)
 
     bind[ElasticsearchService].asEagerSingleton()
 
-    bind[HttpClient].toInstance(
-      HttpClient(
-        ElasticsearchClientUri(host, port),
-        new RequestConfigCallback {
-          override def customizeRequestConfig(builder: RequestConfig.Builder) = {
-            ProxiedRequest
-              .readConfig(cfg)
-              .map { p =>
-                builder
-                  .setProxy(
-                    new HttpHost(
-                      p.host,
-                      p.port,
-                      p.protocol.getOrElse(HttpHost.DEFAULT_SCHEME_NAME)
-                    )
-                  )
-                  .setAuthenticationEnabled(false)
-              }
-              .getOrElse(builder)
-          }
-        }
-      )
-    )
-
+    bind[HttpClient].toInstance(HttpClient(ElasticsearchClientUri(host, port)))
   }
 
 }
