e7c85f3fd609dc4aa6c86ffcb74bd698338e0b68
Ingar Abrahamsen <ingar@abrahams1.com>
2017-08-18 12:46:39 +0200

    feat: improve logging from es sink

--- service_backend/app/services/elasticsearch/shared/ElasticsearchSink.scala
+++ service_backend/app/services/elasticsearch/shared/ElasticsearchSink.scala
@@ -5,12 +5,17 @@ import akka.actor.ActorSystem
 import akka.stream.scaladsl.Sink
 import com.sksamuel.elastic4s.bulk.BulkCompatibleDefinition
 import com.sksamuel.elastic4s.http.HttpClient
-import com.sksamuel.elastic4s.streams.BulkIndexingSubscriber
+import com.sksamuel.elastic4s.http.bulk.BulkResponseItem
+import com.sksamuel.elastic4s.streams.{
+  BulkIndexingSubscriber,
+  RequestBuilder,
+  ResponseListener
+}
 import com.sksamuel.elastic4s.streams.ReactiveElastic._
-import com.sksamuel.elastic4s.streams.RequestBuilder
 import models.elasticsearch.{IndexCallback, IndexConfig, IndexName}
 import no.uio.musit.time
 import org.joda.time.DateTime
+import play.api.Logger
 import repositories.core.dao.IndexStatusDao
 import services.elasticsearch.IndexMaintainer
 
@@ -21,7 +26,11 @@ trait ElasticsearchSink {
   def client: HttpClient
 
   def onComplete: () => Unit
+
   def onError: Throwable => Unit
+
+  def responseListener: ResponseListener[BulkCompatibleDefinition] =
+    ResponseListener.noop
 
   private[this] implicit val rb = new RequestBuilder[BulkCompatibleDefinition] {
     override def request(t: BulkCompatibleDefinition) = t
@@ -33,7 +42,8 @@ trait ElasticsearchSink {
     val sub: BulkIndexingSubscriber[BulkCompatibleDefinition] =
       client.subscriber[BulkCompatibleDefinition](
         completionFn = onComplete,
-        errorFn = onError
+        errorFn = onError,
+        listener = responseListener
       )
     Sink.fromSubscriber(sub)
   }
@@ -50,19 +60,25 @@ class DatabaseMaintainedElasticSearchIndexSink(
     indexConfig: IndexConfig,
     indexCallback: IndexCallback
 )(implicit ec: ExecutionContext)
-    extends ElasticsearchSink {
+    extends ElasticsearchSink
+    with ResponseLogger {
 
+  val logger              = Logger(classOf[DatabaseMaintainedElasticSearchIndexSink])
   val startTime: DateTime = time.dateTimeNow
 
   override def onComplete: () => Unit = () => {
     indexMaintainer.activateIndex(indexConfig.indexName, indexConfig.alias)
     indexStatusDao.indexed(indexConfig.alias, startTime)
     indexCallback.success(IndexName(indexConfig.indexName))
+    logger.info(s"Indexing done for alias ${indexConfig.alias}")
   }
 
-  override def onError: (Throwable) => Unit =
-    _ => indexCallback.failure()
+  override def onError: (Throwable) => Unit = { t =>
+    indexCallback.failure()
+    logger.error(s"Indexing failed for alias ${indexConfig.alias}", t)
+  }
 
+  override def responseListener = this
 }
 
 /**
@@ -75,7 +91,10 @@ class DatabaseMaintainedElasticSearchUpdateIndexSink(
     indexConfig: IndexConfig,
     indexCallback: IndexCallback
 )(implicit ec: ExecutionContext)
-    extends ElasticsearchSink {
+    extends ElasticsearchSink
+    with ResponseLogger {
+
+  val logger = Logger(classOf[DatabaseMaintainedElasticSearchUpdateIndexSink])
 
   val startTime: DateTime = time.dateTimeNow
 
@@ -88,4 +107,25 @@ class DatabaseMaintainedElasticSearchUpdateIndexSink(
 
   override def onError: (Throwable) => Unit =
     _ => indexCallback.failure()
+
+  override def responseListener = this
+}
+
+trait ResponseLogger extends ResponseListener[BulkCompatibleDefinition] {
+
+  def logger: Logger
+
+  override def onAck(resp: BulkResponseItem, org: BulkCompatibleDefinition): Unit = ()
+
+  override def onFailure(
+      resp: BulkResponseItem,
+      org: BulkCompatibleDefinition
+  ): Unit = {
+    if (resp.error.isDefined) {
+      logger.error(s"$resp")
+    } else {
+      logger.warn(s"Failure without error message: $resp")
+    }
+  }
+
 }
