6ac240e69a524484dcfcd94b547b6ae80aee9c2b
Knut Petter Meen <knutpm@usit.uio.no>
2017-09-15 23:51:59 +0200

    Need to revert previously eager deletion :-/

--- service_backend/app/controllers/package.scala
+++ service_backend/app/controllers/package.scala
@@ -134,4 +134,59 @@ package object controllers {
 
   type SimpleNode = (StorageNodeDatabaseId, String)
 
+  /**
+   * TODO: This is quite dirty... should be improved when time allows.
+   */
+  def parseCollectionIdsParam(
+      mid: MuseumId,
+      str: String
+  )(implicit currUsr: AuthenticatedUser): Either[Result, Seq[MuseumCollection]] = {
+    val colIds = str
+      .split(",")
+      .map { ss =>
+        val s = ss.trim
+        (s, CollectionUUID.fromString(s))
+      }
+      .toSeq
+    val badCols = colIds.filter(_._2.isEmpty)
+
+    if (str.isEmpty) {
+      Left(
+        Results.BadRequest(
+          Json.obj(
+            "message" -> s"collectionIds param must contain at least 1 collection ID."
+          )
+        )
+      )
+    } else if (badCols.nonEmpty) {
+      Left(
+        Results.BadRequest(
+          Json.obj(
+            "message" -> s"Invalid CollectionUUIDs: ${badCols.map(_._1).mkString(", ")}"
+          )
+        )
+      )
+    } else {
+      val cids = colIds
+        .filter(_._2.nonEmpty)
+        .flatMap(_._2)
+        .filter(cid => currUsr.canAccess(mid, cid))
+
+      val cols = currUsr.collectionsFor(mid).filter(mc => cids.contains(mc.uuid))
+
+      if (cols.size < colIds.size && !currUsr.hasGodMode) {
+        Left(
+          Results.Forbidden(
+            Json.obj(
+              "message" -> (s"Not authorized to access one or more of " +
+                s" the following collection(s) $str")
+            )
+          )
+        )
+      } else {
+        Right(cols)
+      }
+    }
+  }
+
 }
