3abb0450f1499dfd84614fcf70886de8c22c0821
Jarle Stabell <jarle.stabell@gmail.com>
2018-08-02 17:38:41 +0200

    commented out the extra loopback address

--- docker/dev/pre-deploy.sh
+++ docker/dev/pre-deploy.sh
@@ -2,6 +2,7 @@
 # the default was too little and made it stop.
 sudo sysctl -w vm.max_map_count=262144
 
-#Adds a new loopback address, a hack to let the nginx container talk to the node-js-backend running on localhost (on the container host)
-#https://superuser.com/questions/1089092/how-to-add-a-second-local-loop-back-address
-sudo ip addr add 192.168.40.1/32 dev lo
+# Do this if you  want to have the nodejs backend running directly on the host, instead of in a containter (then you also need to change nginx.conf)
+# Adds a new loopback address, a hack to let the nginx container talk to the node-js-backend running on localhost (on the container host)
+# sudo ip addr add 192.168.40.1/32 dev lo
+# https://superuser.com/questions/1089092/how-to-add-a-second-local-loop-back-address
